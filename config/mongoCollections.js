e3f1669d30fd8ef36b7c1e5b000ca8b0889c82dd
Katie Ng <ngkatie16@gmail.com>
2023-10-19 13:25:40 -0400

    Set up database

---
+++ config/mongoCollections.js
@@ -0,0 +1,20 @@
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// users collection
+export const users = getCollectionFn('users');
+
+// clubs collection
+export const clubs = getCollectionFn('clubs');
