5fc3f9de177d7e0f04c9ba6bb7548c02cad25a05
Vivian <vyam@stevens.edu>
2023-12-06 01:53:55 -0500

    added images, created the explore tab, and updated the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { AuthProvider } from './context/AuthContext'
 import './App.css'
 
 import Landing from './components/Landing'
+import Explore from './components/Explore'
 import Registration from './components/Registration'
 import Login from './components/Login'
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />}/>
         <Route path="/registration" element={<Registration />} />
+        <Route path="/explore" element={<Explore />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </AuthProvider>
--- src/components/Nav2.jsx
+++ src/components/Nav2.jsx
@@ -1,8 +1,7 @@
-import { Box, Typography } from "@mui/material";
-
+import { Box } from "@mui/material";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import ClubFindrLogo from "../../public/logo.png"
+import ClubFindrLogo from "../../public/logo.png";
 import { AuthContext } from "../context/AuthContext";
 import { doSignOut } from "../firebase/FirebaseFunctions";
 
@@ -15,12 +14,12 @@ function Nav() {
         alignItems: "center", 
         width: "100%", 
         height: "120px", 
-        color: "black", // Ensures all text is black
+        color: "black",
         position: "fixed", 
         top: 0, 
         left: 0, 
         zIndex: 1,
-        backgroundColor: 'lightgrey', // Sets the background color to light grey
+        backgroundColor: 'lightgrey',
       }}
     >
         <Box sx={{
@@ -30,26 +29,24 @@ function Nav() {
             justifyContent: "space-between", 
             alignItems: "center", 
             borderBottom: "1px solid rgba(0, 0, 0, 0.2)", 
-            paddingBottom: 2 
+            paddingBottom: 2
           }}
         >
-            <Box
-                component="img"
-                src={ClubFindrLogo}
-                alt="Club Findr Logo"
-                sx={{
-                    width: 50,
-                    height: 50,
-                    paddingLeft: 5,
-                }}
-            />
+            <Box sx={{ paddingLeft: 5 }}>
+                <img
+                    src={ClubFindrLogo}
+                    alt="Club Findr Logo"
+                    style={{
+                        width: 50,
+                        height: 50
+                    }}
+                />
+            </Box>
             <Box sx={{ paddingRight: 5 }}>
-                <Link to="/profile" style={{ marginRight: 20, color: 'black' }}>Profile</Link>
-                <Link to="/explore" style={{ marginRight: 20, color: 'black' }}>Explore</Link>
                 {
                   currentUser
                   ? <Link to="/" onClick={doSignOut} style={{ marginRight: 50, color: 'black' }}>Logout</Link>
-                  : <Link to="/login" style={{ marginRight: 50, color: 'black' }}>Login</Link>
+                  : null // No link displayed if there is no current user
                 }
             </Box>
         </Box>
